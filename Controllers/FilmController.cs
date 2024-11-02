f4801ee3d3c2620b11e3c0555a0a124716d2dc63
piotrMatla <piotrmatla@yahoo.pl>
2024-11-02 13:57:22 +0100

    create a sample list of movies

--- Controllers/FilmController.cs
+++ Controllers/FilmController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FilmowaVS.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmowaVS.Controllers
 {
     public class FilmController : Controller
     {
+        private static IList<Film> films = new List<Film>
+        {
+            new Film(){Id = 1, Name = "Film1", Description = "Desc1", Price = 4},
+            new Film(){Id = 2, Name = "Film2", Description = "Desc2", Price = 5},
+            new Film(){Id = 3, Name = "Film3", Description = "Desc3", Price = 3}
+        };
+
         // GET: FilmController
         public ActionResult Index()
         {
