017666584bd2f63c05e716cd0f12bbd22116b46e
piotrMatla <piotrmatla@yahoo.pl>
2024-11-02 13:20:43 +0100

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FilmowaVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
